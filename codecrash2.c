4d1d84ef17029b911880bb5fc286661e82fe1502
Rayeesf <127919703+Rayeesf@users.noreply.github.com>
2023-03-16 10:42:45 +0530

    Create codecrash2.c

---
+++ codecrash2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    // Write C code here
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            printf("#");
+        }
+    printf("\n");
+    }    
+
+    return 0;
+}
